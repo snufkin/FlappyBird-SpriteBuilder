32a53089f4b8f0be8b820f43c6a9787d70f93781
Balazs Dianiska <balazs.dianiska@acquia.com>
2014-11-23 15:23:08 +0100

    Automatically Committed for MakeGamesWithUs

--- FlappyBird.spritebuilder/Source/MainScene.m
+++ FlappyBird.spritebuilder/Source/MainScene.m
@@ -169,7 +169,7 @@
     for (CCNode *cloud in _clouds) {
         // Make the clouds float, vary its height by \pm 0.1 cloud y position.
         float newPosition;
-        newPosition = cloud.position.y + 0.001 * sin(100 * character.physicsBody.velocity.x * delta) * cloud.position.y;
+        newPosition = cloud.position.y + 0.001 * sin(10 * character.physicsBody.velocity.x * delta) * cloud.position.y;
         cloud.position = ccp(cloud.position.x - (character.physicsBody.velocity.x * delta), newPosition);
         
         // Loop the cloud if its out of the screen.
