6b7721b32be84b9347f0fa4dc42a6e9e0408ed70
Balazs Dianiska <balazs.dianiska@acquia.com>
2014-11-23 15:19:32 +0100

    Automatically Committed for MakeGamesWithUs

--- FlappyBird.spritebuilder/Source/MainScene.m
+++ FlappyBird.spritebuilder/Source/MainScene.m
@@ -169,7 +169,9 @@
     for (CCNode *cloud in _clouds) {
         cloud.position = ccp(cloud.position.x - (character.physicsBody.velocity.x * delta), cloud.position.y);
         if (cloud.position.x <= (-1 * cloud.contentSize.width)) {
-            cloud.position = ccp(cloud.position.x + 2 * cloud.contentSize.width, cloud.position.y);
+            // Make the clouds float, vary its height by \pm 0.1 cloud y position.
+            float newPosition = cloud.position.y + 0.1 sin(character.physicsBody.velocity.x * delta) * cloud.position.y;
+            cloud.position = ccp(cloud.position.x + 2 * cloud.contentSize.width, newPosition);
         }
     }
     
